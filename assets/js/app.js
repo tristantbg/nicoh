ec621b1fe38453a121fae254e91d0f8a2240bffd
Tristan <tristan.bagot@gmail.com>
2017-04-03 15:56:35 +0200

    commit message

---
+++ assets/js/app.js
@@ -0,0 +1,68 @@
+/* globals $:false */
+var width = $(window).width(),
+    height = $(window).height(),
+    isMobile = false,
+    $root = '/';
+$(function() {
+    var app = {
+        init: function() {
+            $(window).resize(function(event) {});
+            $(document).ready(function($) {
+                $body = $('body');
+                History.Adapter.bind(window, 'statechange', function() {
+                    var State = History.getState();
+                    console.log(State);
+                    var content = State.data;
+                    if (content.type == 'project') {
+                        $body.addClass('project loading');
+                        app.loadContent(State.url + '/ajax', slidecontainer);
+                    }
+                });
+                $(document).keyup(function(e) {
+                    //esc
+                    if (e.keyCode === 27) app.goIndex();
+                    //left
+                    if (e.keyCode === 37 && $slider) app.goPrev($slider);
+                    //right
+                    if (e.keyCode === 39 && $slider) app.goNext($slider);
+                });
+                $(window).load(function() {
+                    $(".loader").fadeOut("fast");
+                });
+            });
+        },
+        sizeSet: function() {
+            width = $(window).width();
+            height = $(window).height();
+            if (width <= 770 || Modernizr.touch) isMobile = true;
+            if (isMobile) {
+                if (width >= 770) {
+                    //location.reload();
+                    isMobile = false;
+                }
+            }
+        },
+        goIndex: function() {
+            History.pushState({
+                type: 'index'
+            }, $sitetitle, window.location.origin + $root);
+        },
+        loadContent: function(url, target) {
+            $.ajax({
+                url: url,
+                success: function(data) {
+                    $(target).html(data);
+                }
+            });
+        },
+        deferImages: function() {
+            var imgDefer = document.getElementsByTagName('img');
+            for (var i = 0; i < imgDefer.length; i++) {
+                if (imgDefer[i].getAttribute('data-src')) {
+                    imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-src'));
+                }
+            }
+        }
+    };
+    app.init();
+});
